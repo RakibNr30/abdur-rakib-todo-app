dc9604616a6f90ccd965f84ab6b06f17b3973c87
Abdur Rakib <rakib.cse14th@gmail.com>
2024-04-01 12:55:13 +0600

    toast bottom to top

--- src/components/DefaultToast.jsx
+++ src/components/DefaultToast.jsx
@@ -2,7 +2,7 @@ import {Toast, ToastContainer} from "react-bootstrap";
 
 const DefaultToast = ({show, setShow, title, body, variant}) => {
     return (
-        <ToastContainer position="bottom-end" className="p-3" style={{ zIndex: 1 }}>
+        <ToastContainer position="top-end" className="p-3" style={{ zIndex: 1 }}>
             <Toast bg={variant} onClose={() => setShow(false)} show={show} delay={3000} autohide>
                 <Toast.Header>
                     <strong className="me-auto">{title}</strong>
